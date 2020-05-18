987bf4cf0233dfb903e06bc5387eb9aa58ba2307
tobiasfaust <tobias.faust@gmx.net>
2020-05-18 12:17:57 +0200

    Update test.ino

--- test/test.ino
+++ test/test.ino
@@ -1,4 +1,4 @@
-#include <Wire.h>
+/*#include <Wire.h>
 #include "PCF8574.h"
 #include "WEMOS_Motor.h"
 #include "SSD1306Wire.h"
@@ -13,7 +13,7 @@
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 #include "uptime.h"
 #include "i2cdetect.h"
-
+*/
 
 void setup() {
   // put your setup code here, to run once:
