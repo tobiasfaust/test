2749c8a2266d800967f1127323fb9ff57fdd129b
tobiasfaust <tobias.faust@gmx.net>
2020-06-04 09:15:57 +0200

    Create ChangeLog.h

---
+++ ChangeLog.h
@@ -0,0 +1,11 @@
+Release 2.4.3:  
+	- Bugfixing Automatische Releaseverteilung
+    - Überarbeitung Github Workflow mit automatischer Releaseerstellung 
+
+Release 2.4.2:  
+	- Bugfixing des TB6612 Handlings
+	- erste Version der automatischen Releaseverteilung
+
+Release 2.4.1:  
+	- xxxx
+
