e2e3a149c1215d968cc06f4d3e605b3ac200198b
tobiasfaust <tobias.faust@gmx.net>
2021-01-19 09:55:32 +0100

    .

--- test.ino
+++ test.ino
@@ -1,20 +1,16 @@
 /*
 #include <Wire.h>  
+#include "uptime.h"
+#include "i2cdetect.h"
 #include "PCF8574.h"
 #include "WEMOS_Motor.h"
 #include "SSD1306Wire.h"
 #include <ArduinoJson.h>
-#include <i2cdetect.h>
-#include <DNSServer.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266HTTPUpdateServer.h>
-#include <ESP8266mDNS.h>
 #include <PubSubClient.h>
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 #include "uptime.h"
 #include "i2cdetect.h"
 */ 
+
 
 void setup() {
   Serial.begin(115200);
