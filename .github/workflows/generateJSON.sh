b977e468cea572f044c9dbbc4d498a3c0e66589b
Tobias Faust <tobias.faust@gmx.net>
2024-11-08 14:58:58 +0100

    update

--- .github/workflows/generateJSON.sh
+++ .github/workflows/generateJSON.sh
@@ -78,6 +78,15 @@ do
   BINARYFILENAME=$(basename $FILENAME"."$ARCH".v"$VERSION"-"$SUBVERSION"."$STAGE)
   DOWNLOADURL="http://tfa-releases.s3-website.eu-central-1.amazonaws.com/"$REPOSITORYNAME"/"$BINARYFILENAME"."$FILEEXT
 
+  if [[ -f $BINARYPATH"/merged_"$(basename $FILENAME)"."$FILEEXT ]]; then
+    FULLFILENAME="merged_"$BINARYFILENAME
+    FULLFILE_URL="http://tfa-releases.s3-website.eu-central-1.amazonaws.com/"$REPOSITORYNAME"/"$FULLFILENAME"."$FILEEXT
+  elif
+    echo "merged file not found at "$BINARYPATH"/merged_"$(basename $FILENAME)"."$FILEEXT
+    FULLFILENAME=""
+    FULLFILE_URL=""
+  fi
+
   JSON='      {
   "name":"Release '$VERSION'-'$STAGE'",
   "version":"'$VERSION'",
@@ -85,7 +94,8 @@ do
   "number":'$NUMBER',
   "stage":"'$STAGE'",
   "arch":"'$ARCH'",
-  "download-url":"'$DOWNLOADURL'"
+  "download-url":"'$DOWNLOADURL'",
+  "url_fullfile":"'$FULLFILE_URL'"
       }'
 
   echo -e "\n\n"$GREEN"Echo json string"$NC
@@ -93,6 +103,10 @@ do
 
   echo $JSON > $RELEASEPATH/$BINARYFILENAME".json"
   cp $FILE $RELEASEPATH/$BINARYFILENAME"."$FILEEXT
+  
+  if [[ -f $BINARYPATH"/merged_"$(basename $FILENAME)"."$FILEEXT ]]; then
+    cp $BINARYPATH"/merged_"$(basename $FILENAME)"."$FILEEXT $RELEASEPATH/$FULLFILENAME"."$FILEEXT
+  fi
 
 done
 
