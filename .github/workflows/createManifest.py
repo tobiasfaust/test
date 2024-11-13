016963a9e4f1270b0ea50bb4079a10aac34e52fe
Tobias Faust <tobias.faust@gmx.net>
2024-11-13 20:52:23 +0100

    update

--- .github/workflows/createManifest.py
+++ .github/workflows/createManifest.py
@@ -1,32 +1,30 @@
-import argparse
+import argparse, os
 from pathlib import Path
 from myUtils import *
 
 parser = argparse.ArgumentParser()
 
 # erwartete Parameter
-parser.add_argument('--RootDir', type=str, help='Root Verzeichnis für alle Firmwares', default='web-installer/firmware')
-parser.add_argument('--FwDir', type=str, help='Verzeichnis wo eine bestimmte Firware liegt', default='web-installer/firmware/v2.5.0-208-DEV')
+parser.add_argument('--FwDir', type=str, help='Root Verzeichnis für alle Firmwares', default='web-installer/firmware')
+parser.add_argument('--VersDir', type=str, help='Verzeichnis wo eine bestimmte Firware liegt', default='web-installer/firmware/v2.5.0-208-DEV')
 
 # Parsen der Argumente
 args = parser.parse_args()
 
-if Path(args.FwDir).is_dir() and Path(args.RootDir).is_dir():
+if Path(args.VersDir).is_dir() and Path(args.FwDir).is_dir():
     # Benennen die Verzeichnisse um, die noch ein zip (-> Artifacts) als Endung haben
-    renameDirs(args.FwDir)
+    renameDirs(args.VersDir)
 
     # erstelle das Manifest.json aller ESP Architekturen im Hauptverzeichnis der Version
-    process_manifests(args.FwDir)
-
-
-    output_file = 'versions.json'  # Ausgabedatei im Root-Verzeichnis
+    process_manifests(args.VersDir)
 
     # Suche nach manifest.json-Dateien und extrahiere die relevanten Informationen
-    extracted_data = search_manifests_and_extract_version(args.RootDir)
+    extracted_data = search_manifests_and_extract_version(args.FwDir)
         
     # Wenn Daten extrahiert wurden, speichere sie in einer neuen JSON-Datei
     if extracted_data:
-        save_results_to_json(extracted_data, output_file)
+        save_results_to_json(extracted_data, os.path.join(args.FwDir, 'versions.json'))
     else:
         print("Keine relevanten 'manifest_all.json' Dateien gefunden.")
-print(f"Der Pfad {args.FwDir} oder {args.RootDir} ist nicht verfügbar")
+else:
+    print(f"Der Pfad {args.VersDir} oder {args.FwDir} ist nicht verfügbar")
