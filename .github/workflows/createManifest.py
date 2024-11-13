1ef7849432d3421f64f6623408bd06bf1c6bbeae
Tobias Faust <tobias.faust@gmx.net>
2024-11-13 15:07:25 +0100

    update

--- .github/workflows/createManifest.py
+++ .github/workflows/createManifest.py
@@ -5,12 +5,13 @@ from myUtils import *
 parser = argparse.ArgumentParser()
 
 # erwartete Parameter
-parser.add_argument('--FwDir', type=str, help='Verzeichnis wo eine bestimmte Firware liegt', default='firmware/v2.5.0-208-DEV')
+parser.add_argument('--RootDir', type=str, help='Root Verzeichnis für alle Firmwares', default='web-installer/firmware')
+parser.add_argument('--FwDir', type=str, help='Verzeichnis wo eine bestimmte Firware liegt', default='web-installer/firmware/v2.5.0-208-DEV')
 
 # Parsen der Argumente
 args = parser.parse_args()
 
-if Path(args.FwDir).is_dir():
+if Path(args.FwDir).is_dir() and Path(args.RootDir).is_dir():
     # Benennen die Verzeichnisse um, die noch ein zip (-> Artifacts) als Endung haben
     renameDirs(args.FwDir)
 
@@ -21,11 +22,11 @@ if Path(args.FwDir).is_dir():
     output_file = 'versions.json'  # Ausgabedatei im Root-Verzeichnis
 
     # Suche nach manifest.json-Dateien und extrahiere die relevanten Informationen
-    extracted_data = search_manifests_and_extract_version("firmware")
+    extracted_data = search_manifests_and_extract_version(args.RootDir)
         
     # Wenn Daten extrahiert wurden, speichere sie in einer neuen JSON-Datei
     if extracted_data:
         save_results_to_json(extracted_data, output_file)
     else:
         print("Keine relevanten 'manifest_all.json' Dateien gefunden.")
-print(f"Der Pfad {args.FwDir} ist nicht verfügbar")
+print(f"Der Pfad {args.FwDir} oder {args.RootDir} ist nicht verfügbar")
