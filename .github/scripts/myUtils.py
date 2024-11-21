d84b60515bfa1e8ed83df12292e37fe3bcc3deab
Tobias Faust <tobias.faust@gmx.net>
2024-11-21 19:00:55 +0100

    update

--- .github/scripts/myUtils.py
+++ .github/scripts/myUtils.py
@@ -261,7 +261,7 @@ def deleteVersions(root: str, keepVersions: int, versions: list = None) -> None:
         for build in sorted_builds[:-keepVersions]:
             for path in versions[stage][build]:
                 # Lösche den Ordner
-                shutil.rmtree({path})
+                shutil.rmtree(f'web-installer/{path}')
                 logging.info(f"{path} gelöscht")
     
 
