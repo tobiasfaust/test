438e4d69171cf17143890f6e215494292de677a8
Tobias Faust <tobias.faust@gmx.net>
2024-11-25 16:24:35 +0100

    update index.html and javascript.js to use dynamic repository name and enhance version handling

--- javascript.js
+++ javascript.js
@@ -2,13 +2,16 @@
 window.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
-    fetch('firmware/versions.json')
-    .then(response => response.json())
-    .then(data => {
-        const versions = data;
-        GenerateSelectList(versions);
+    Promise.all([
+        fetch('firmware/versions.json').then(response => response.json()),
+        fetch('firmware/releases.json').then(response => response.json())
+    ])
+    .then(([versions, releases]) => {
+        GenerateSelectList(versions, releases);
+        // Process releases if needed
         checkSupported(); 
         resetCheckboxes();
+        handleTemplates();
     })
     .catch(error => console.error('Error loading versions:', error));
 }
@@ -23,16 +26,24 @@ function showSerialHelp() {
     `;
 }
 
+function getRepositoryName() {
+    const url = new URL(top.location.href);
+    const pathSegments = url.pathname.split('/');
+    const firstPathSegment = pathSegments.length > 1 ? pathSegments[1] : '';
+    return firstPathSegment;
+}
+
 function checkSupported() {
     if (document.getElementById('web-install-button').hasAttribute('install-unsupported')) unsupported();
     else setManifest();
 }
 
 function unsupported() {
+    
     document.getElementById('flasher').innerHTML = `Sorry, your browser is not yet supported!<br>
     Please try on Desktop Chrome or Edge.<br>
     Find binary files here:<br>
-    <a href="https://github.com/tobiasfaust/WLED/releases" target="_blank">
+    <a href="https://github.com/tobiasfaust/` + getRepositoryName() + `/releases" target="_blank">
     <button class="btn" slot="activate">GitHub Releases</button>
     </a>`
 }
@@ -46,16 +57,26 @@ function resetCheckboxes() {
 }
 
 // json: {path: 'firmware/v2.5.0-237-development/manifest_all.json', version: 'v2.5.0-237', stage: 'development', build: 237}
-function GenerateSelectList(json) {
+function GenerateSelectList(versions, releases) {
     const select = document.getElementById('versions');
     const stages = {};
 
-    // Group by stage
-    json.forEach(obj => {
-        if (!stages[obj.stage]) {
-            stages[obj.stage] = [];
+    // Group by stage, only include development versions
+    versions.forEach(obj => {
+        if (obj.stage == "development") {
+            if (!stages[obj.stage]) {
+                stages[obj.stage] = [];
+            }
+            stages[obj.stage].push(obj);
         }
-        stages[obj.stage].push(obj);
+    });
+
+    // Group by stage, for all releases
+    releases.forEach(obj => {
+        if (!stages[obj.stage]) {
+                stages[obj.stage] = [];
+            }
+            stages[obj.stage].push(obj);
     });
 
     // Sort each stage by build number in descending order
@@ -84,4 +105,17 @@ function setManifest() {
     document.getElementById('web-install-button').setAttribute('manifest', m);
 }
 
+function handleTemplates() {
+    const elem = {"template": "REPOSITORY", value: getRepositoryName() };
+    
+    // gehe durch elem durch und suche in allen Objekten mit innerHTML den value aus "template" 
+    // eingeschlossen in {{ }} und ersetze diesen mit dem Wert aus value
 
+    document.querySelectorAll('*').forEach(node => {
+        if (node.innerHTML.includes('{{' + elem.template + '}}')) {
+            while (node.innerHTML.includes('{{' + elem.template + '}}')) {
+                node.innerHTML = node.innerHTML.replace('{{' + elem.template + '}}', elem.value);
+            }
+        }
+    });
+}
